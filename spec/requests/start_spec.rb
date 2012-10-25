cf433e1af89ffd4da0ffd73d7e7b88a7a8e5149e
Hari Ananth <hari.ananth@moovweb.com>
2012-10-25 01:00:32 -0700

    fix test

--- spec/requests/start_spec.rb
+++ spec/requests/start_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe "Start" do
   describe "GET /start" do
-    it "display the game state" do
-      visit start_path
+    it "displays the game state" do
+      visit root_path
       page.should have_content 'Welcome to Literature'
     end
   end
