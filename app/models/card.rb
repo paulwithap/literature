42f57ec5c402ee626f2512408eddc9e600c1f4c4
Hari Ananth <hari.ananth@moovweb.com>
2012-10-25 01:00:28 -0700

    add relationship for cards -> user/team

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,4 @@
 class Card < ActiveRecord::Base
   attr_accessible :number, :suit
+  belongs_to :owner, :polymorphic => true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
 class Team < ActiveRecord::Base
   attr_accessible :name, :points
+  has_many :cards, :as => :owner
 end
